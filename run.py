3a1876ad7df0a212c51d3a9b8a05bd81e41f8bc5
Vinay Kumar Maheshwaram <54681400+sudovinay01@users.noreply.github.com>
2025-07-12 07:14:36 +0000

    Add initial script to open YouTube channel in web browser

---
+++ run.py
@@ -0,0 +1,8 @@
+import webbrowser
+
+print("Hello, World!")
+print("Welcome to the YouTube Channel ....")
+print("Opening YouTube channel: @sudovinay01")
+
+url = "https://www.youtube.com/@sudovinay01"
+webbrowser.open(url)
